733252b1f64b747deb84596579e3ad5ed2f92cb1
Varun Sharma <coding.with.binaries@gmail.com>
2020-09-08 12:40:09 +0530

    Initial commit

---
+++ timezone-manager-server/src/main/java/com/manager/timezone/timezonemanagerserver/TimezoneManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.manager.timezone.timezonemanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezoneManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezoneManagerServerApplication.class, args);
+	}
+
+}
---
+++ timezone-manager-server/src/test/java/com/manager/timezone/timezonemanagerserver/TimezoneManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.timezone.timezonemanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
